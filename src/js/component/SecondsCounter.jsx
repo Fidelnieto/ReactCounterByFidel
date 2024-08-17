2415262198bf3b0cc3eceaa83722535a6fa5f373
FidelNieto <nietofidel25@gmail.com>
2024-08-16 21:50:35 -0400

    adding SecondsCounterjsx

---
+++ src/js/component/SecondsCounter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SecondCounter = (props) => {
+  const icon = <i className="fa-solid fa-paint-roller"></i>;
+
+  return (
+    <div className="">
+      <i className="fa-brands fa-instagram"></i>
+    </div>
+  );
+};
+
+export default SecondCounter;
